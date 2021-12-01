9932365987384f956e4c5a5a767ff49c4630fc1c
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-01 12:40:54 -0500

    Update day1.js

--- day-1/day1.js
+++ day-1/day1.js
@@ -1,8 +1,15 @@
 //Here we go!!
 
 const data = "day1-data.txt";
+var arr = [];
+var count = 0;
 
-fetch(data)
+fetch (data)
   .then(txt => txt.text())
-  .then(str => str.split("\n"))
-  .then(parsed => console.log(parsed));
+  .then(str => arr = str.split("\n"));
+
+for (let i = 1; i < arr.length - 1; i++) {
+  if (arr[i] > arr[i - 1]) count++;
+}
+
+console.log("Result: " + count);
