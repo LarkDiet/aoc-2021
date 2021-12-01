d0f47fd5e86eb2e4f858af743911db2d1b84a149
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-01 12:45:16 -0500

    Update day1.js

--- day-1/day1.js
+++ day-1/day1.js
@@ -8,7 +8,8 @@ fetch (data)
   .then(txt => txt.text())
   .then(str => arr = str.split("\n"));
 
-for (let i = 1; i < arr.length - 1; i++) {
+//Cut 2 bc the last value in arr is blank
+for (let i = 1; i < arr.length - 2; i++) {
   if (arr[i] > arr[i - 1]) count++;
 }
 
