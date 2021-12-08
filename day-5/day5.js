624a36718bc2b3acc7d8d2d376e26f71029b6f85
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-08 17:52:02 -0500

    Update day5.js

--- day-5/day5.js
+++ day-5/day5.js
@@ -1,4 +1,5 @@
 //Day 5
+//please don't run this it crashed my ide xcvbzxcvn
 
 const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-5/day5-data.txt';
 
@@ -9,10 +10,32 @@ async function parseData() {
   return arr;
 }
 
-async function findOverlaps() {
+async function findHVLines() {
 	let dataArr = await parseData();
-  let lineArr = [];
+  let hvLines = [];
   for (let i = 0; i < dataArr.length; i++) {
+  	let coords = dataArr[i].split(/\D/g).filter(v => v);
+    console.log(coords);
+    let x1 = coords[0];
+    let y1 = coords[1];
+    let x2 = coords[2];
+    let y2 = coords[3];
+    
+    if (x1 == x2) {
+    	hvLines.push(Array.from(Array(Math.abs(y1 - y2)), (_, i) => [x1, Math.min(y1, y2) + i]));
+    } else if (y1 == y2) {
+    	hvLines.push(Array.from(Array(Math.abs(x1 - x2)), (_, i) => [y1, Math.min(x1, x2) + i]));
+    }
+  }
+  return hvLines;
+}
+
+findHVLines().then(console.log);
+
+async function findOverlaps() {
+	let lines = await findHVLines();
+  let ovPoints = [];
+  for (let i = 1; i < lines.length; i++) {
   	let line;
     
   }
