7d5c573622cff15ac776ce56d82c413fcca0e580
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-09 18:59:56 -0500

    Update day5.js

--- day-5/day5.js
+++ day-5/day5.js
@@ -18,7 +18,7 @@ async function findHVLines() {
     let y1 = coords[1];
     let x2 = coords[2];
     let y2 = coords[3];
-    if (x1 == x2 || y1 == y2) hvLines.push(coords);
+    if (x1 == x2 || y1 == y2) hvLines.push(coords.map(v => parseInt(v)));
   }
   return hvLines;
 }
@@ -27,10 +27,22 @@ findHVLines().then(console.log);
 
 async function findOverlaps() {
 	let lines = await findHVLines();
+  let mainMatrix = Array(1000).fill(Array(1000).fill(0));
   let ovPoints = [];
-  for (let i = 1; i < lines.length; i++) {
-  	let cline = lines[i];
-    let pline = lines[i - 1];
-    if (cline)
+  for (let line of lines) {
+  	let lineX1 = line[0];
+    let lineY1 = line[1];
+    let lineX2 = line[2];
+    let lineY2 = line[3];
+  	if (lineY1 == lineY2) {
+    	mainMatrix[lineY1] = mainMatrix[lineY1].map(v, i => {
+      	if (i <= Math.max(lineX1, lineX2) && i >= Math.min(lineX1, lineX2)) v++;
+        });
+    } else {
+    	for (let i = Math.min(lineY1, lineY2); i <= Math.max(lineY1, lineY2); i++) {
+      	mainMatrix[i][lineX1] = mainMatrix[i][lineX1] + 1;
+      }
+    }
   }
+  //...
 }
