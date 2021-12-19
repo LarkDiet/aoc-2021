eab23eb3fdf5d3ea9ac1843bfd02d44b9a738fbd
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-19 10:30:49 -0500

    Update day9.js

--- day-9/day9.js
+++ day-9/day9.js
@@ -1,6 +1,7 @@
 //Day 9
 
-const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-9/day9-data.txt';
+//const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-9/day9-data.txt';
+const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-9/sample-data.txt';
 
 async function parseData() {
   let file = await fetch(input);
@@ -9,30 +10,31 @@ async function parseData() {
   return arr;
 }
 
-async function findLowsAndSum() {
+async function findLows() {
 	let dataArr = await parseData();
   let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
   let lowPoints = [];
-  for (let y of dataMatrix) {
-  	for (let x of dataMatrix) {
+  for (let y = 0; y < dataMatrix.length; y++) {
+  	for (let x = 0; x < dataMatrix[y].length; x++) {
     	let cLoc = dataMatrix[y][x];
-  		let compArr = [cLoc];
+      let compArr = [dataMatrix[y][x - 1], dataMatrix[y][x + 1]].filter(v => v != undefined);
+      if (y != 0) compArr.push(dataMatrix[y - 1][x]);
+      if (y != dataMatrix.length - 1) compArr.push(dataMatrix[y + 1][x]);
+      if (compArr.every(v => v > cLoc)) lowPoints.push([y, x]);
     }
   }
-  
-  for (let i = 0; i < dataArr.length; i ++) {
-  	let pLocArr = [];
-    let nLocArr = [];
-  	let cLocArr = dataArr[i].split('').map(v => parseInt(v));
-    if (i != 0) pLocArr = dataArr[i - 1].split('').map(v => parseInt(v));
-    if (i != dataArr.length - 1) nLocArr = dataArr[i + 1].split('').map(v => parseInt(v));
-		//A low point is any element of cLocArr that is the min of
-    //c[i], p[i], n[i], c[i-1], c[i+1] (given all are !undefined)
-    let compArr = [cLocArr[i]];
-    let tryArr = [pLocArr[i], nLocArr[i], cLocArr[i - 1], cLocArr[i + 1]];
-    for (let ele of tryArr) if (ele != undefined) compArr.push(ele);
-    return compArr;
-  }
+  return lowPoints;
 }
 
-findLowsAndSum().then(console.log);
+async function riskAndSum() {
+	let dataArr = await parseData();
+  let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
+	let lpArr = await findLows();
+  return lpArr.map(yx => dataMatrix[yx[0]][yx[1]] + 1).reduce((pv, cv) => pv + cv, 0);
+}
+
+async function findBasins() {
+
+}
+
+riskAndSum().then(console.log);
