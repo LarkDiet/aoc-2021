b4a1d07d97b07be04f37a5daa640db195e507157
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-19 12:19:32 -0500

    Update day9.js

--- day-9/day9.js
+++ day-9/day9.js
@@ -11,12 +11,12 @@ async function parseData() {
 }
 
 async function findLows() {
-	let dataArr = await parseData();
+  let dataArr = await parseData();
   let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
   let lowPoints = [];
   for (let y = 0; y < dataMatrix.length; y++) {
-  	for (let x = 0; x < dataMatrix[y].length; x++) {
-    	let cLoc = dataMatrix[y][x];
+    for (let x = 0; x < dataMatrix[y].length; x++) {
+      let cLoc = dataMatrix[y][x];
       let compArr = [dataMatrix[y][x - 1], dataMatrix[y][x + 1]].filter(v => v != undefined);
       if (y != 0) compArr.push(dataMatrix[y - 1][x]);
       if (y != dataMatrix.length - 1) compArr.push(dataMatrix[y + 1][x]);
@@ -27,14 +27,35 @@ async function findLows() {
 }
 
 async function riskAndSum() {
-	let dataArr = await parseData();
+  let dataArr = await parseData();
   let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
-	let lpArr = await findLows();
+  let lpArr = await findLows();
   return lpArr.map(yx => dataMatrix[yx[0]][yx[1]] + 1).reduce((pv, cv) => pv + cv, 0);
 }
 
-async function findBasins() {
-
+async function findBasinsAndProd3() {
+  let dataArr = await parseData();
+  let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
+  let lpArr = await findLows();
+  let basins = [];
+  //Coordinates in each basin include those between
+  //Two nines and/or a nine and a matrix edge
+  //That are adjacent to the lp or its adjacent branches
+  for (let lp of lpArr) {
+    let linkArr = [lp];
+    let hasAdj = true;
+    while (hasAdj) {
+      let linkAdj = linkArr.flatMap(yx => {
+        let ma = [dataMatrix[yx[0]][yx[1] - 1], dataMatrix[yx[0]][yx[1] + 1]].filter(v => v != undefined && v != 9);
+        if (![0, 9].includes(yx[0])) ma.push(dataMatrix[yx[0] - 1][yx[1]]);
+        if (![dataMatrix.length - 1, 9].includes(yx[0])) ma.push(dataMatrix[yx[0] + 1][yx[1]]);
+        return ma;
+      });
+      if (linkAdj.length == 0) hasAdj = false;
+      else linkArr = linkArr.concat(linkAdj);
+    }
+    basins.push(linkArr);
+  }
 }
 
 riskAndSum().then(console.log);
