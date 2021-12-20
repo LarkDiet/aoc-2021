3a2a292fa21eccf1b8d1c898cf184d8d20dd278b
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-20 15:47:59 -0500

    Update day9.js

--- day-9/day9.js
+++ day-9/day9.js
@@ -33,6 +33,10 @@ async function riskAndSum() {
   return lpArr.map(yx => dataMatrix[yx[0]][yx[1]] + 1).reduce((pv, cv) => pv + cv, 0);
 }
 
+const includesArray = (data, arr) => {
+	return data.some(e => Array.isArray(e) && e.every((o, i) => Object.is(arr[i], o)));
+}
+
 async function findBasinsAndProd3() {
   let dataArr = await parseData();
   let dataMatrix = dataArr.map(l => l.split('').map(v => parseInt(v)));
@@ -48,25 +52,27 @@ async function findBasinsAndProd3() {
     if (y != dataMatrix.length - 1) adjCoords.push([y + 1, x]);
     adjCoords = adjCoords.filter(ac => dataMatrix[ac[0]][ac[1]] != 9);
     basinCoords = basinCoords.concat(adjCoords);
+    console.log("Basin for " + lp + ": ");
     console.log(basinCoords);
-    while (adjCoords != []) {
+    //while (adjCoords != []) {
     	let newAdjCoords = [];
     	for (let adjc of adjCoords) {
       	let adjToAdjCoords = [];
       	let acy = adjc[0];
         let acx = adjc[1];
-        if (dataMatrix[y][x - 1] != undefined) adjToAdjCoords.push([y, x - 1]);
-    		if (dataMatrix[y][x + 1] != undefined) adjToAdjCoords.push([y, x + 1]);
-    		if (y != 0) adjToAdjCoords.push([y - 1, x]);
-    		if (y != dataMatrix.length - 1) adjToAdjCoords.push([y + 1, x]);
-        adjToAdjCoords = adjToAdjCoords.filter(ac => !newAdjCoords.includes([ac[0], ac[1]]) && !basinCoords.includes([ac[0], ac[1]]) && dataMatrix[ac[0]][ac[1]] != 9);
+        if (dataMatrix[acy][acx - 1] != undefined) adjToAdjCoords.push([acy, acx - 1]);
+    		if (dataMatrix[acy][acx + 1] != undefined) adjToAdjCoords.push([acy, acx + 1]);
+    		if (acy != 0) adjToAdjCoords.push([acy - 1, acx]);
+    		if (acy != dataMatrix.length - 1) adjToAdjCoords.push([acy + 1, acx]);
+        adjToAdjCoords = adjToAdjCoords.filter(ac => !includesArray(newAdjCoords, [ac[0], ac[1]]) && !includesArray(basinCoords, [ac[0], ac[1]]) && dataMatrix[ac[0]][ac[1]] != 9);
         newAdjCoords = newAdjCoords.concat(adjToAdjCoords);
       }
       basinCoords = basinCoords.concat(newAdjCoords);
+      console.log("Basin for " + lp + ": ");
       console.log(basinCoords);
       adjCoords = [...newAdjCoords];
     } 
-  }
+  //}
 }
 
 //riskAndSum().then(console.log);
