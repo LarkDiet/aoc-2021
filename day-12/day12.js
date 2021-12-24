f16298aeb35ac08c08ce2baca94409a20c11c767
Lark Henriquez <nightinglarkdiet@gmail.com>
2021-12-23 20:47:30 -0500

    Update day12.js

--- day-12/day12.js
+++ day-12/day12.js
@@ -1,8 +1,8 @@
 //Day 12
-//Dont look this is a mess and also unfinished xcvbxbn
+//Uhhhhhhhh
 
 //const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-12/day12-data.txt';
-const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-12/sample-data.txt';
+const input = 'https://raw.githubusercontent.com/LarkDiet/aoc-2021/main/day-12/sample-data-3.txt';
 
 async function parseData() {
   let file = await fetch(input);
@@ -14,32 +14,54 @@ async function parseData() {
 async function findValidPaths() {
   let dataArr = await parseData();
   let largeCaves = [...new Set(dataArr.join('-').split('-'))].filter(v => v == v.toUpperCase());
-  console.log(largeCaves);
   let smallCaves = [...new Set(dataArr.join('-').split('-'))].filter(v => v == v.toLowerCase() && !['start', 'end'].includes(v));
-  console.log(smallCaves);
-  let startBranches = dataArr.filter(v => v.includes('start')).join('-').split('-').filter(v => v != 'start').map(v => 'start-' + v);
-  console.log(startBranches);
-  let endBranches = dataArr.filter(v => v.includes('end')).join('-').split('-').filter(v => v != 'end').map(v => v + '-end');
-  console.log(endBranches);
+  let allCaves = smallCaves.concat(largeCaves);
+  console.log(allCaves);
+  let startNodes = dataArr.filter(v => v.includes('start')).join('-').split('-').filter(v => v != 'start');
+  let endNodes = dataArr.filter(v => v.includes('end')).join('-').split('-').filter(v => v != 'end');
+  const outletsOf = (cave) => dataArr.filter(v => v.split('-').includes(cave) && !v.includes('start')).join('-').split('-').filter(v => v != cave);
+  
+  const caveMap = new Map();
+  allCaves.forEach(c => caveMap.set(c, outletsOf(c)));
+  //caveMap.forEach((v,k) => console.log(k + ': ' + v));
+
+	/* Find all combinations that meet these rules
+  	-Starts with a startNode
+    -Ends with an endNode
+    -Follows the continuity rules in the data set
+    -Has at max 1 of each smallCave
+    -Caves where the only outlet is a small cave are excluded
+    */
+  caveMap.forEach((v, k) => {
+  	if (v.length == 1 && smallCaves.includes(v[0])) caveMap.delete(k);
+  });
+  caveMap.forEach((v,k) => console.log(k + ': ' + v));
+  
   let paths = [];
+  caveMap.forEach((v,k) => {
+  	if (startNodes.includes(k)) paths.push(k);
+  })
+  
+
+	/*
+  //From each start branch, find and push the longest legal path/s
+  //Illegal steps are where /every/ outlet is a small cave already in the path
+  //Then push every possible shortcut starting from the end
+  let longPaths = [];
   for (let sb of startBranches) {
-    let currentStep = sb.substr(-2, 2);
-		//Referencing the dataArr, find and evaluate each outlet from end of start branch
-    let outlets = dataArr.filter(v => v.includes(currentStep) && !v.includes('start')).join('-').split('-').filter(v => v != currentStep);
-    for (let outlet of outlets) {
-      if (outlet == 'end')
-        paths.push(sb + '-end');
-      else {
-      	let adjOutlets = dataArr.filter(v => v.includes(outlet) && !v.includes('start')).join('-').split('-').filter(v => v != outlet);
-        for (let adjo of adjOutlets) {
-        	if (adjo == 'end')
-          	paths.push(sb + '-' + outlet + '-end');
-          
-        }
+    let lastStep = sb.substr(-2, 2);
+    let legalPaths = [];
+    let outlets = outletsOf(lastStep).filter(o => o != 'end');
+    for (let o of outlets) {
+    	let newPath = [lastStep, o];
+      while(true) {
+      	lastStep = newPath[newPath.length - 1];
+      	let adjOutlets = outletsOf(lastStep).filter(o => o != 'end' && !outletsOf(o).every(oo => newPath.includes(oo) && smallCaves.includes(oo)));
+        
       }
     }
   }
-  return paths;
+  */
 }
 
 findValidPaths().then(console.log);
